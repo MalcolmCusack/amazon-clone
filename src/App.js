5ab50134a8610dbc33564c8acab0d2a272a95e07
Malcolm Cusack <malcolmcusack@malcolms-mbp.home>
2020-09-04 00:30:58 -0600

    created the router and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <p> yoooo </p>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Router path = "/Login">
+            <h1> Login page </h1>
+          </Router>
+          <Route path="/">
+            <h1> HOME </h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
