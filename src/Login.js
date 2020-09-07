9463bb3be62323adfa0f26c8a32180737f05352b
Malcolm Cusack <malcolmcusack@malcolms-mbp.home>
2020-09-07 15:14:42 -0600

    Creating the login page. Working on auth using firebase Authentication using email and password state Then redirects back to the home page

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Login.css"
+import { Link, useHistory } from "react-router-dom"
+import { auth } from './firebase';
 
 function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const login = event => {
+        event.preventDefault(); //prevents referesh
+
+        auth.signInWithEmailAndPassword(email, password)
+            .then((auth) => {      //checks auth with firebase and sends back to homepage
+                //other wise alerts of wrong auth
+                history.push('/');
+        }).catch(e => alert(e.message));
+    }
+
+    // Authentication => email/pass enable
+
+    const register = event => {
+        event.preventDefault(); //prevents referesh
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(auth => {         //creates new user in firebase db
+
+            }).catch((e) => alert(e.message));
+    }
+
     return (
-        <div>
-            
+        <div className='login'>
+            <Link to='/'>
+                <img
+                    className="login_logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+                    alt=""
+                />
+            </Link>
+
+            <div className="login_container">
+                <h1>Sign in</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input 
+                        value={email} 
+                        onChange={event => setEmail(event.target.value)} 
+                        type="email"
+                    />
+                    <h5>Password</h5>
+                    <input 
+                        value={password} 
+                        type="password" 
+                        onChange={event => setEmail(event.target.value)}
+                        />
+                    <button onClick={login} type="submit">Sign In</button>
+                </form>
+
+                <button onClick={register} type="submit">Create your Amazon Account</button>
+            </div>
         </div>
     )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,15 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-
+    //firebase->project->settings->SDK/config
+    apiKey: "AIzaSyC4JMM6Z5Qn7NnjNCZ7ET3IFXbt5PVqTV4",
+    authDomain: "clone-924c0.firebaseapp.com",
+    databaseURL: "https://clone-924c0.firebaseio.com",
+    projectId: "clone-924c0",
+    storageBucket: "clone-924c0.appspot.com",
+    messagingSenderId: "8777842181",
+    appId: "1:8777842181:web:d96875f38a4841a94c266c",
+    measurementId: "G-C38JGQEMVQ"
 });
 
 const db = firebaseApp.firestore();
