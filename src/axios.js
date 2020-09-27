d5bbab0191edc52b15c351cfa362ac3d51ebdad8
malcolmcusack <cusackmalk@gmail.com>
2020-09-26 19:37:18 -0600

    built and deployed amazon clone!

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 
 const instance = axios.create({
     // API cloud functional URL
-    baseURL: "http://localhost:5001/clone-924c0/us-central1/api"
+    baseURL: "https://us-central1-clone-924c0.cloudfunctions.net/api"
+    //"http://localhost:5001/clone-924c0/us-central1/api"
 });
 
 export default instance;
+
+// firebase deploy --only functions deploys backend
+
+// npm run build
+
+// firebase deploy --only hosting
